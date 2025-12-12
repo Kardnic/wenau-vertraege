81afca76c999aae0ec79af86af52816ca944d0e3
Kardnic <158296649+Kardnic@users.noreply.github.com>
2025-12-12 08:30:00 +0100

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -247,7 +247,7 @@ async function generatePlayerPDF(s) {
   const zusatzField = form.createTextField("zusatzbedingungen");
   zusatzField.enableMultiline();
   zusatzField.setFontSize(10 * SCALE);
-  zusatzField.setText(""); 
+  zusatzField.setText("Test"); 
   zusatzField.addToPage(page, {
     x: marginLeft,
     y: y - fieldHeight,
