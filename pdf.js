af821f401fe08fe5a165ad309b45bb49ee74bf05
Kardnic <158296649+Kardnic@users.noreply.github.com>
2026-04-25 15:37:08 +0200

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -164,8 +164,7 @@ async function generatePlayerPDF(s) {
   const rowHeight = 18 * SCALE;
 
   const rows = [
-    ["Gehalt/Jahr (€)", Number(s.gehalt_jahr || 0).toFixed(2)],
-    ["Gehalt/Monat (€)", Number(s.gehalt_monat || 0).toFixed(2)],
+    ["max. möglich/Monat (€)", Number(s.gehalt_monat || 0).toFixed(2)],
     ["Monatliche Prämie (€)", Number(s.fahrtgeld || 0).toFixed(2)],
     ["Trainingsprämie (€)", Number(s.trainingspraemie || 0).toFixed(2)],
     ["Spielprämie (€)", Number(s.spielpraemie || 0).toFixed(2)],
